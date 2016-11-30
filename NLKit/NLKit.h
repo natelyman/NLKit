1fc73d17f61602700bbe771167b2464670c2a768
Nate Lyman <nlyman@fanatics.com>
2016-11-29 18:22:26 -0800

    Initial Commit

---
+++ NLKit/NLKit.h
@@ -0,0 +1,13 @@
+//
+//  NLKit.h
+//  NLKit
+//
+//  Created by Nate Lyman on 11/29/16.
+//  Copyright © 2016 Nate Lyman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLKit : NSObject
+
+@end
---
+++ NLKit/NLKit.m
@@ -0,0 +1,13 @@
+//
+//  NLKit.m
+//  NLKit
+//
+//  Created by Nate Lyman on 11/29/16.
+//  Copyright © 2016 Nate Lyman. All rights reserved.
+//
+
+#import "NLKit.h"
+
+@implementation NLKit
+
+@end
