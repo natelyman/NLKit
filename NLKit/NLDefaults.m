e6e1c0d856d2e9cd9e68c03d390aff6647f5558e
Nate Lyman <nlyman@Nates-MacBook-Pro.local>
2017-01-22 00:01:25 -0800

    Changes to NLDefaults

--- NLKit/NLDefaults.m
+++ NLKit/NLDefaults.m
@@ -8,50 +8,76 @@
 
 #import "NLDefaults.h"
 
+@interface NLDefaults()
+@property (nonatomic,strong) NSUserDefaults *defaults;
+@end
+
 @implementation NLDefaults
 
++ (instancetype)sharedDefaults {
+    static NLDefaults *__sharedDefaults;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        __sharedDefaults = [[NLDefaults alloc] init];
+    });
+    return __sharedDefaults;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if(self) {
+        self.defaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.natelyman.Affiliate"];
+    }
+    return self;
+}
+
++ (NSUserDefaults *)standardDefaults {
+    return [[NLDefaults sharedDefaults] defaults];
+}
+
 + (BOOL)boolForKey:(NSString *)key {
-    return [[NSUserDefaults standardUserDefaults] boolForKey:key];
+    return [[NLDefaults standardDefaults] boolForKey:key];
 }
 
 + (void)setBool:(BOOL)value forKey:(NSString *)key {
-    [[NSUserDefaults standardUserDefaults] setBool:value forKey:key];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [[NLDefaults standardDefaults] setBool:value forKey:key];
+    [[NLDefaults standardDefaults] synchronize];
 }
 
 + (NSString *)stringForKey:(NSString *)key {
-    return [[NSUserDefaults standardUserDefaults] stringForKey:key];
+    return [[NLDefaults standardDefaults] stringForKey:key];
 }
 
 + (void)setString:(NSString *)value forKey:(NSString *)key {
-    [[NSUserDefaults standardUserDefaults] setObject:value forKey:key];
+    [[NLDefaults standardDefaults] setObject:value forKey:key];
 }
 
 + (NSNumber *)numberForKey:(NSString *)key {
-    return (NSNumber *)[[NSUserDefaults standardUserDefaults] objectForKey:key];
+    return (NSNumber *)[[NLDefaults standardDefaults] objectForKey:key];
 }
 
 + (void)setNumber:(NSNumber *)value forKey:(NSString *)key {
-    [[NSUserDefaults standardUserDefaults] setObject:value forKey:key];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [[NLDefaults standardDefaults] setObject:value forKey:key];
+    [[NLDefaults standardDefaults] synchronize];
 }
 
 + (NSInteger)integerForKey:(NSString *)key {
-    return [[NSUserDefaults standardUserDefaults] integerForKey:key];
+    return [[NLDefaults standardDefaults] integerForKey:key];
 }
 
 + (void)setInteger:(NSInteger)value forKey:(NSString *)key {
-    [[NSUserDefaults standardUserDefaults] setInteger:value forKey:key];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [[NLDefaults standardDefaults] setInteger:value forKey:key];
+    [[NLDefaults standardDefaults] synchronize];
 }
 
 + (float)floatForKey:(NSString *)key {
-    return [[NSUserDefaults standardUserDefaults] floatForKey:key];
+    return [[NLDefaults standardDefaults] floatForKey:key];
 }
 
 + (void)setFloat:(float)value forKey:(NSString *)key {
-    [[NSUserDefaults standardUserDefaults] setFloat:value forKey:key];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [[NLDefaults standardDefaults] setFloat:value forKey:key];
+    [[NLDefaults standardDefaults] synchronize];
 }
 
 @end
